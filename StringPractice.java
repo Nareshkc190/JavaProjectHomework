45ba2ebe865d63b1d66efaa5a17eca140445b844
Nareshkc190 <nareshkc1.9.0@gmail.com>
2018-08-08 11:05:09 -0400

    Initial commit

---
+++ StringPractice.java
@@ -0,0 +1,22 @@
+
+public class StringPractice {
+	
+	
+	public static void main(String[] args) {
+		String h = "hello";
+		String h2 = "hello";
+		System.out.println(h);
+		System.out.println(h2);
+		h2 = "string";
+		System.out.println(h);
+		System.out.println(h2);
+		
+		StringBuilder s = new StringBuilder("change this");
+		s.replace(2,3, "d");
+		System.out.println(s);
+		
+		s.trimToSize();
+    	System.out.println(s);
+	}
+
+}
